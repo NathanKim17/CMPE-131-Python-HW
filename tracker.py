65badc6676e89d076fe02254fb4762a7709a6305
Nathan Kim <nkimfromhome@gmail.com>
2022-03-10 20:05:16 -0800

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -1,1 +1,23 @@
-print()
+def func_counter(some_func):
+    def counter_func(y):
+        some_func(y)
+        counter_func.counter = counter_func.counter + 1
+
+    counter_func.counter = 0
+    return counter_func
+
+@func_counter
+def foo(y):
+    return y+2**3-34
+
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+foo(1)
+
+print(foo.counter)
